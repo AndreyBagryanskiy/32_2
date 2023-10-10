73f948d0f83435b33c7fe2cc34f0a2ab82389114
Andrey Bagryanskiy <Andrey@1dist.ru>
2023-10-10 15:59:15 +0300

    finished

--- main.cpp
+++ main.cpp
@@ -4,12 +4,28 @@
 #include"nlohmann/json.hpp"
 
 int main(){
+    std::string actor;
+    std::cout << "Enter the actor's first and last name: ";
+    std::getline(std::cin, actor);
+
     std::ifstream file("films.json");
 
     nlohmann::json films;
     file >> films;
     file.close();
-
-    std::cout << films["Deadpool"]["Actors"]["Ed Skrein"];
- 
+    int findcount = 0;
+    for(nlohmann::json::iterator it = films.begin(); it != films.end(); ++it){
+        nlohmann::json film = *it;
+        for(nlohmann::json::iterator itf = film.begin(); itf != film.end(); ++itf){
+            nlohmann::json actors = *itf;
+            if (actors.find(actor) != actors.end()) {
+                ++findcount;
+                std::cout << film["Film name"] << std:: endl;
+                std::cout << actor << " as " << std::string(actors[actor]) << std::endl;
+                std::cout << std::endl;
+            }
+        }
+        
+    }
+    if(findcount == 0) std::cout << "Films not found!";
 }
